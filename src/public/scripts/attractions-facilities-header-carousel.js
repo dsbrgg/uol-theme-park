0d5a10742fddadf5c61823fbaee5e44870f780af
JerryHamm <106247774+JerryHamm@users.noreply.github.com>
2022-06-25 10:35:02 -0600

    minor changes 15

--- src/public/scripts/attractions-facilities-header-carousel.js
+++ src/public/scripts/attractions-facilities-header-carousel.js
@@ -18,11 +18,9 @@ function showSlides() {
     if (slideIndex > slides.length) 
     {
         slideIndex = 1
-    }
+    } 
 
     slides[slideIndex-1].style.display = "block";
-
-    dots[slideIndex-1].className += " attractions-facilities-header-carousel-active";
 
     setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
